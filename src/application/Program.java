387ba02d8561efbb33b12b700c237669be8c4a9e
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-05-30 11:42:50 -0300

    Implementando Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
     static void main() {
 
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
