445584334d579a759549ac4297e0a5fe737cab35
Vinicius Fernandes <vinicius.fernandes.vfl19@gmail.com>
2026-06-04 21:03:47 -0300

    Implementando movimentos possíveis da Torre

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (chessPiece) capturedPiece;
     }
@@ -46,6 +47,12 @@ public class ChessMatch {
         }
         if (!board.piece(position).isTheAnyPossibleMoves()) {
             throw new chessException("There is no possible moves for the chosen piece");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new chessException("The chosen piece can't move to target position");
         }
     }
 
--- src/chess/chessPiece.java
+++ src/chess/chessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class chessPiece extends Piece {
 
@@ -14,5 +15,10 @@ public abstract class chessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        chessPiece p = (chessPiece) getBoard().piece(position);
+        return p != null && p.getColor() != color;
     }
 }
